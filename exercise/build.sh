88c19c58b8b90fadfdf7ba298e0ae573039f3e68
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2024-05-28 15:40:27 +0900

    done exercise/

--- exercise/build.sh
+++ exercise/build.sh
@@ -5,5 +5,5 @@
 readonly BASE_DIR=$(cd $(dirname $0); pwd)
 readonly BASENAME="$(basename $0)"
 
-$BASE_DIR/../../shared/build/build_foreach.sh $BASE_DIR $@
+$BASE_DIR/../deep/build/build_foreach.sh $BASE_DIR $@
 # @@@ sample end
