d55c7f60dd435a0172cb6bb30548ebf03943acf9
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2024-06-07 14:28:56 +0900

    change strucutre directorys of headrs to export.

---
+++ example/programming_convention/pkg.cpp
@@ -0,0 +1,10 @@
+// @@@ sample begin 0:0
+
+#include <memory>
+#include <mutex>
+#include <string>
+
+#include "PackageA/xxx.h"      // PackageAのインクルード
+#include "PackageB/yyy.h"      // PackageBのインクルード
+#include "internal.h"          // パッケージ外部非公開ヘッダのインクルード
+// @@@ sample end
