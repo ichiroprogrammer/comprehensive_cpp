d370022c18fefa26affa905f9d3a29f34ef15ee3
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2024-06-21 15:46:53 +0900

    enhance export dir

--- example/programming_convention/pkg.cpp
+++ example/programming_convention/pkg.cpp
@@ -1,10 +1,17 @@
 // @@@ sample begin 0:0
 
-#include <memory>
-#include <mutex>
-#include <string>
+// PackageC内の*.cppファイルの内部とする
+#include <memory>  // システムヘッダ
+#include <mutex>   // システムヘッダ
+#include <string>  // システムヘッダ
 
+// PackageBがエクスポートするするヘッダを使用するために、
+// 以下のようなコンパイルオプションが必要になる
+//      -I<DIR>/PackageA/h
+// <DIR>はコンパイラが実行されるディレクトリ
+//
 #include "PackageA/xxx.h"  // PackageAのインクルード
-#include "PackageB/yyy.h"  // PackageBのインクルード
+#include "PackageB/b0.h"   // PackageBのインクルード
+#include "PackageB/b3.h"   // PackageBのインクルード
 #include "internal.h"      // パッケージ外部非公開ヘッダのインクルード
 // @@@ sample end
