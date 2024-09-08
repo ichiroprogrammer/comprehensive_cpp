48ffd8b63823069ec9c2a011e922746cf1cd0acd
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2024-09-08 17:34:55 +0900

    add c++14 features sample

--- example/cpp_improve/cpp14_ut.cpp
+++ example/cpp_improve/cpp14_ut.cpp
@@ -1,3 +1,9 @@
+#include <chrono>
+#include <complex>
+#include <sstream>
+#include <tuple>
+#include <utility>
+
 #include "gtest_wrapper.h"
 
 namespace {
@@ -63,4 +69,93 @@ TEST(cpp14, template_val)
     // @@@ sample end
 }
 
+// @@@ sample begin 3:0
+
+template <typename Tuple, std::size_t... I>
+std::string tuple2string(const Tuple& t, std::index_sequence<I...>)
+{
+    std::ostringstream os;
+
+    ((os << std::get<I>(t) << " "), ...);  // Tupleの各要素を出力
+
+    return os.str();
+}
+
+template <typename... Args>
+std::string process_tuple(const std::tuple<Args...>& t)
+{
+    return tuple2string(t, std::index_sequence_for<Args...>{});
+}
+// @@@ sample end
+
+TEST(cpp14, integer_sequence)
+{
+    // @@@ sample begin 3:1
+
+    std::tuple<int, double, const char*> t(1, 2.5, "test");
+
+    auto ret = process_tuple(t);  // タプルの要素を出力 (1, 2.5, test)
+    ASSERT_EQ("1 2.5 test ", ret);
+    ASSERT_EQ(1, std::get<0>(t));
+    ASSERT_EQ(2.5, std::get<1>(t));
+    ASSERT_STREQ("test", std::get<2>(t));
+    // @@@ sample end
+}
+
+TEST(cpp14, tupple_get)
+{
+    // @@@ sample begin 4:0
+
+    std::tuple<int, float, std::string> my_tuple(1, 3.14f, "hello");
+
+    // 異なる型のオーバーロードされた std::get を使用
+    ASSERT_EQ(std::get<int>(my_tuple), 1);
+    ASSERT_FLOAT_EQ(std::get<float>(my_tuple), 3.14f);
+    ASSERT_EQ(std::get<std::string>(my_tuple), "hello");
+    // @@@ sample end
+}
+
+TEST(cpp14, s_literal)
+{
+    // @@@ sample begin 5:0
+
+    using namespace std::string_literals;  // "s" リテラルを使用
+
+    std::string str = "Hello"s + ", World!";
+
+    ASSERT_EQ(str, "Hello, World!");
+    // @@@ sample end
+}
+
+TEST(cpp14, chrono_literal)
+{
+    // @@@ sample begin 6:0
+
+    using namespace std::chrono_literals;
+
+    auto ten_seconds  = 10s;
+    auto five_minutes = 5min;
+    auto half_hour    = 0.5h;
+
+    ASSERT_EQ(ten_seconds.count(), 10);
+    ASSERT_EQ(five_minutes.count(), 5);
+    ASSERT_DOUBLE_EQ(half_hour.count(), 0.5);
+    // @@@ sample end
+}
+
+TEST(cpp14, complex_literal)
+{
+    // @@@ sample begin 7:0
+
+    using namespace std::complex_literals;
+
+    std::complex<double> c1 = 1.0 + 2i;
+    std::complex<double> c2 = 2.0 - 3i;
+
+    ASSERT_DOUBLE_EQ(c1.real(), 1.0);
+    ASSERT_DOUBLE_EQ(c1.imag(), 2.0);
+    ASSERT_DOUBLE_EQ(c2.real(), 2.0);
+    ASSERT_DOUBLE_EQ(c2.imag(), -3.0);
+    // @@@ sample end
+}
 }  // namespace
